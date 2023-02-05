6583f10fb4160b250f908117cc36c114b693bb1e
Ivan <volkov19@mail.ru>
2023-02-05 12:40:38 +0400

    Create simple client-server

---
+++ src/main/java/edu/javacourse/net/Server.java
@@ -0,0 +1,74 @@
+package edu.javacourse.net;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ServerSocket socket = new ServerSocket(25225, 2000);
+
+        System.out.println("Server is started");
+        while (true) {
+            Socket client = socket.accept();
+            new SimpleServer(client).start();
+        }
+    }
+
+}
+
+class SimpleServer extends Thread {
+
+    private Socket client;
+
+    public SimpleServer(Socket client) {
+        this.client = client;
+    }
+
+    public void run() {
+        handleRequest();
+    }
+
+    private void handleRequest() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+            String request = br.readLine();
+            String [] lines = request.split("\\s+");
+            String command = lines[0];
+            String userName = lines[1];
+            System.out.println("Server got string 1: " + command);
+            System.out.println("Server got string 2: " + userName);
+//        Thread.sleep(2000);
+
+            String response = buildResponse(command, userName);
+            bw.write(response);
+            bw.newLine();
+            bw.flush();
+
+            br.close();
+            bw.close();
+
+            client.close();
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    private String buildResponse(String command, String userName) {
+        switch(command){
+            case "HELLO" : return "Hello, " + userName;
+            case "MORNING" : return "Good morning, " + userName;
+            case "DAY" : return "Good day, " + userName;
+            case "EVENING" : return "Good day, " + userName;
+            default: return "Hi, " + userName;
+        }
+
+    }
+}
